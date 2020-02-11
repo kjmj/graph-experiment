0e161e4a305e7e36d5863924ac71ffb511f9830a
Ken Morton Jr <kmorton15@gmail.com>
2020-02-11 15:42:39 -0500

    added d3 and a basic graph for graph 1

--- src/components/experimentComponents/Graph1.js
+++ src/components/experimentComponents/Graph1.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import * as d3 from 'd3';
 
-function Graph1() {
-  return (
-      <div className="Graph1">
-        <p>Graph1 Component</p>
-      </div>
-  );
+class Graph1 extends React.Component {
+  componentDidMount() {
+    const data = [ 2, 4, 2, 6, 8 ];
+    this.drawBarChart(data)
+  }
+  drawBarChart(data)  {
+    const canvasHeight = 400;
+    const canvasWidth = 600;
+    const scale = 20;
+    const svgCanvas = d3.select(this.refs.canvas)
+        .append("svg")
+        .attr("width", canvasWidth)
+        .attr("height", canvasHeight)
+        .style("border", "1px solid black");
+    svgCanvas.selectAll("rect")
+        .data(data).enter().append("rect")
+        .attr("width", 40)
+        .attr("height", (datapoint) => datapoint * scale)
+        .attr("fill", "orange")
+        .attr("x", (datapoint, iteration) => iteration * 45)
+        .attr("y", (datapoint) => canvasHeight - datapoint * scale);
+  }
+  render() { return <div ref="canvas"><p>Graph 1 Component</p></div> }
 }
 
 export default Graph1;
