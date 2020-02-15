4ed9b39af4852ce2e41c5d2fe5f3beb5703e45b4
Neer Jariwala <njariwala@gmail.com>
2020-02-15 15:17:29 -0500

    updated instructions

--- src/components/experimentComponents/Description.js
+++ src/components/experimentComponents/Description.js
@@ -10,9 +10,11 @@ class Description extends React.Component {
     return (
         <div className="Description">
           <h1>Welcome to our Experiment!</h1>
-          <h3>You will see bar, pie, and bubble charts.</h3>
-          <h3>Please insert the what percent the smaller marked bar/slice/bubble is of the bigger marked
-            bar/slice/bubble.</h3>
+          <h3>You will bar, pie, and stacked bar charts.</h3>
+          <h3>Please enter what percent the smaller marked bar/slice is of the bigger marked
+            bar/slice into the text box as an integer.</h3>
+            <h3>Note it is possible that the bar/slices can be the same size. In this case,
+                100 is an appropriate answer.</h3>
           <h3>Thank You!</h3>
           <Button onClick={this.saveAndContinue}>Start</Button>
         </div>
