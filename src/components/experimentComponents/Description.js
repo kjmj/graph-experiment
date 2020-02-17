ef502753e54664328de8d2a29b7a72e7877072e3
Ken Morton Jr <kmorton15@gmail.com>
2020-02-17 14:10:19 -0500

    added information on home and description page

--- src/components/experimentComponents/Description.js
+++ src/components/experimentComponents/Description.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Button} from "react-bootstrap";
+
 class Description extends React.Component {
   saveAndContinue = (e) => {
     e.preventDefault();
@@ -9,13 +10,13 @@ class Description extends React.Component {
   render() {
     return (
         <div className="Description">
-          <h1>Welcome to our Experiment!</h1>
-          <h3>You will see bar, pie, and stacked bar charts.</h3>
-          <h3>Please enter what percent the smaller marked bar/slice is of the bigger marked
-            bar/slice into the text box as an integer.</h3>
-            <h3>Note it is possible that the bar/slices can be the same size. In this case,
-                100 is an appropriate answer.</h3>
-          <h3>Thank You!</h3>
+          <h2>Welcome to our Experiment!</h2>
+          <h4>You will see bar, pie, and stacked bar charts.</h4>
+          <h4>Please enter what percent the smaller marked bar/slice is of the bigger marked
+            bar/slice into the text box as an integer.</h4>
+          <h4>Note it is possible that the bar/slices can be the same size. In this case,
+            100 is an appropriate answer.</h4>
+          <h4>Thank You!</h4>
           <Button onClick={this.saveAndContinue}>Start</Button>
         </div>
     );
