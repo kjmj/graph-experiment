9a095785ba4b57729008adffe3665df82883ec54
Ken Morton Jr <kmorton15@gmail.com>
2020-03-06 15:36:15 -0500

    update github link

--- src/components/navPages/Home.js
+++ src/components/navPages/Home.js
@@ -4,11 +4,11 @@ function Home() {
   return (
       <div className="Home">
         <h1>CS480x Project 3</h1>
-        <p>We designed a simple experiment to determine if bar, pie, or stacked bar charts are better</p>
+        <p>We designed a simple experiment to determine if bar, pie, or stacked bar charts are better.</p>
         <p>This project served as an intro to react, and we were able to get a better understanding of how to use
-          component based frameworks</p>
+          component based frameworks.</p>
         <p>If you'd like to learn more about our findings or view our source code, check out <a
-            href="https://github.com/kjmj/03-Experiment" target="_blank">our GitHub repo</a></p>
+            href="https://github.com/kjmj/graph-experiment" target="_blank">our GitHub repo.</a></p>
         <p>Click on the experiment tab to start!</p>
       </div>
   );
